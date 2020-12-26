2157d1ec78e1ee8b754ab1f213a26fe7732a898e
kerinlin <18296613547@qq.com>
2020-12-26 23:24:49 +0800

    自动弹出目标文件夹的位置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -224,7 +224,7 @@ export default {
         }
         let timer = setTimeout(async () => {
           await this.removeDir(dirPath);
-          // shell.showItemInFolder(targetDirPath);
+          shell.showItemInFolder(targetDirPath);
           this.loading = false;
           clearTimeout(timer);
         }, 1000);
